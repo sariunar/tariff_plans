39f9b024b73f95d3acf5de027d35187baee9a782
sariunar <rinchinovasariuna@gmail.com>
2023-04-09 21:29:51 +0800

    add click tariff

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,10 +4,10 @@ import '../TariffCard/Tariff.css'
 import Tariff from '../TariffCard/Tariff';
 
 const tariffPlans = [
-  { name: "Безлимитный 300", price: "300", speed: "до 10 Мбит/сек", description: "Объем включенного трафика не ограничен" },
-  { name: "Безлимитный 450", price: "450", speed: "до 50 Мбит/сек", description: "Объем включенного трафика не ограничен" },
-  { name: "Безлимитный 550", price: "550", speed: "до 100 Мбит/сек", description: "Объем включенного трафика не ограничен", isSelected: true },
-  { name: "Безлимитный 1000", price: "1000", speed: "до 200 Мбит/сек", description: "Объем включенного трафика не ограничен" }
+  { id: 1, name: "Безлимитный 300", price: "300", speed: "до 10 Мбит/сек", description: "Объем включенного трафика не ограничен" },
+  { id: 2, name: "Безлимитный 450", price: "450", speed: "до 50 Мбит/сек", description: "Объем включенного трафика не ограничен" },
+  { id: 3, name: "Безлимитный 550", price: "550", speed: "до 100 Мбит/сек", description: "Объем включенного трафика не ограничен" },
+  { id: 4, name: "Безлимитный 1000", price: "1000", speed: "до 200 Мбит/сек", description: "Объем включенного трафика не ограничен" }
 ];
 class App extends React.Component {
   render() {
@@ -16,7 +16,7 @@ class App extends React.Component {
         <div className="App">
           {
             tariffPlans.map((tariff) =>
-              <Tariff name={tariff.name} price={tariff.price} speed={tariff.speed} description={tariff.description} isSelected={tariff.isSelected} />
+              <Tariff key={tariff.id} name={tariff.name} price={tariff.price} speed={tariff.speed} description={tariff.description} isSelected={tariff.isSelected} />
             ) // вставляются данные из json как в массиве
           }
         </div>
--- src/components/TariffCard/Tariff.jsx
+++ src/components/TariffCard/Tariff.jsx
@@ -1,18 +1,19 @@
 import './Tariff.css';
-import React from 'react';
-
-class Tariff extends React.Component {
-    render() {
-        const { name, price, speed, description, isSelected } = this.props;
-        return (
-            <div className={'tariff-card ' + (isSelected ? "selected" : "")}>
-                <div className='tariff-card__name'>{name}</div>
-                <div className='tariff-card__price'><span>{price}</span> руб/мес</div>
-                <div className='tariff-card__speed'>{speed}</div>
-                <div className='tariff-card__description'>{description}</div>
-            </div>
-        );
+import React, { useState } from 'react';
+const Tariff = ({ name, price, speed, description }) => {
+    const [selected, setSelected] = useState(false);
+    const handleClick = () => {
+        setSelected(!selected);
+        //как сделать, чтобы тариф возвращался в изначальную форму не только при клику по ней же, но и при клике за ее пределами 
     }
+    return (
+        <div className={'tariff-card ' + (selected ? "selected" : "")} onClick={handleClick}>
+            <div className='tariff-card__name'>{name}</div>
+            <div className='tariff-card__price'><span>{price}</span> руб/мес</div>
+            <div className='tariff-card__speed'>{speed}</div>
+            <div className='tariff-card__description'>{description}</div>
+        </div>
+    );
 }
 
 export default Tariff;
